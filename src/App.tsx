08362b067ed8c7c49ba95215f1252c2fc26e9f51
zhanghaojie <1455123087@qq.com>
2024-03-04 10:37:35 +0800

    Add echarts library and create a bar chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
+import * as echarts from 'echarts';
 import './App.css';
+import { useEffect, useRef } from 'react';
 
 function App() {
-  return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold font-mono">hello</h1>
-      </div>
-    </>
-  );
+  const chartRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    const myChart = echarts.init(chartRef.current!);
+    myChart.setOption({
+      title: {
+        text: 'ECharts Getting Started Example',
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['shirt', 'cardigan', 'chiffon', 'pants', 'heels', 'socks'],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: 'sales',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    });
+  }, []);
+  return <div ref={chartRef} className="h-full" />;
 }
 
 export default App;
